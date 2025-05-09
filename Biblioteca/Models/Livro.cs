4ddcf4bdf342390f6293afce8938be36e1fcc2a0
Prof. Cristiano <cristiano.paula@docente.senai.br>
2025-05-09 07:55:44 -0300

    Adicionado as Models Usuario, Livro e Genero

---
+++ Biblioteca/Models/Livro.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Biblioteca.Models
+{
+    public class Livro
+    {
+        public int LivroId { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Descricao { get; set; }
+        public string Editora { get; set; }
+        public DateOnly DataPublicacao { get; set; }
+        public int NumeroPaginas { get; set; }
+        public int Quantidade { get; set; }
+        public string? UrlCapa { get; set; }
+        public string? ISBN10 { get; set; }
+        public string? ISBN13 { get; set; }
+
+        public int GeneroId { get; set; }
+        public Genero? Genero { get; set; }
+    }
+}
---
+++ Biblioteca/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+
+namespace Biblioteca.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string NomeCompleto { get; set; }
+        public string CPF { get; set; }
+        public string Celular { get; set; }
+        public string DataNascimento { get; set; }
+        public string? UrlFoto { get; set; }
+
+        // Relacionamento com o IdentityUser
+        public Guid? AppUserId { get; set; }
+        public IdentityUser? IdentityUser { get; set; }
+    }
+}
